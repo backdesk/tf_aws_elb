42a4f5022a9bc9ae75160d6010040c297ffec552
James Gardner <james.gardner@uk.tesco.com>
2017-05-08 11:44:12 +0100

    Removed provider configuration

--- elb_http/variables.tf
+++ elb_http/variables.tf
@@ -8,7 +8,8 @@ variable "elb_name" {}
 
 variable "elb_is_internal" {
   description = "Determines if the ELB is internal or not"
-  default = false
+  default     = false
+
   // Defaults to false, which results in an external IP for the ELB
 }
 
@@ -27,21 +28,18 @@ variable "backend_port" {
 }
 
 variable "backend_protocol" {
- description = "The protocol the backend service speaks"
- // Possible options are
- // - http
- // - https
- // - tcp
- // - ssl (secure tcp)
+  description = "The protocol the backend service speaks"
+
+  // Possible options are
+  // - http
+  // - https
+  // - tcp
+  // - ssl (secure tcp)
 }
 
 variable "health_check_target" {
   description = "The URL the ELB should use for health checks"
+
   // This is primarily used with `http` or `https` backend protocols
   // The format is like `HTTPS:443/health`
 }
-
-// Variables for providers used in this module
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
